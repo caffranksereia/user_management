678dd1ef31c6bd3ff7c66497541db70632abecd5
Caffrankseria <circuncisaofabio@icloud.com>
2024-06-19 09:34:16 -0300

    feat: add usermanagement

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserManagement } from './user_management/user_management.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UserManagement],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
