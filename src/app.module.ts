43f1f1522d3b53e598a18e92982deb1fd6d52b29
Caffrankseria <circuncisaofabio@icloud.com>
2024-06-19 11:55:55 -0300

    feat: conexao db import usermanagemententity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
 import { UserManagement } from './user_management/user_management.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserManageEntity } from './user_management/entities/user_management.entity';
 
 @Module({
-  imports: [UserManagement],
+  imports: [ UserManagement,
+    TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'Mari0001',
+    database: 'postgres',
+    entities: [
+      UserManageEntity,
+    ],
+    synchronize: true
+  })],
   controllers: [],
   providers: [],
 })
