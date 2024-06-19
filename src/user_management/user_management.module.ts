196f845d31b32712295e2411a144c6305d522d19
Caffrankseria <circuncisaofabio@icloud.com>
2024-06-19 09:34:40 -0300

    feat: controller e service

---
+++ src/user_management/user_management.module.ts
@@ -0,0 +1,11 @@
+import { Module } from "@nestjs/common";
+import { UserManagementController } from "./user_management.controller";
+import { UserManagementService } from "./user_management.service";
+
+@Module({
+  imports:[],
+  controllers:[UserManagementController],
+  providers:[UserManagementService],
+
+})
+export class UserManagement {}
